3ee48c5e8d964dc99ff96914e0c87f8616fb878e
Zloteanu Nichita <nichita.zloteanu@gmail.com>
2021-08-12 13:58:15 +0300

    Attempting to fix

--- src/sms/sms.module.ts
+++ src/sms/sms.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TwilioModule } from 'nestjs-twilio';
 import { PrismaModule } from 'src/prisma/prisma.module';
-import { UserModule } from 'src/user/user.module';
 import { SmsController } from './sms.controller';
 import { SmsService } from './sms.service';
 if (process.env.NODE_ENV !== 'production') require('dotenv').config(); // This fixes env variables on dev
+
+// console.log("Got envs:")
+// console.log(process.env)
 
 @Module({
   imports: [TwilioModule.forRoot({
