cd71708dd115111ff7e3392cc69496e1406a172a
Cristi Pele <cristi_pele7@yahoo.com>
2021-10-28 16:26:56 +0300

    push notifications

--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -1,9 +1,8 @@
-import { BadRequestException, Injectable, Logger } from '@nestjs/common';
-import { Prisma, Notification, User, NotificationSource } from '@prisma/client';
+import { Injectable, Logger } from '@nestjs/common';
+import { Prisma, Notification } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UserService } from 'src/user/user.service';
 import { NotificationPushService } from './notification.push.service';
-import * as PushNotifications from 'node-pushnotifications';
 import { ShallowUserSelect } from 'src/prisma-queries-helper/shallow-user-select';
 
 @Injectable()
@@ -85,147 +84,13 @@ export class NotificationService {
     data: Prisma.NotificationCreateInput,
     silent?: boolean,
   ): Promise<Notification> {
-    const sSilent = silent ?? false;
-    if (!sSilent) {
-      this.pushSingleNotification(data);
-    }
+    // const sSilent = silent ?? false;
+    // if (!sSilent) {
+    //   this.pushService.pushNotification(data);
+    // }
     return this.prisma.notification.create({
       data,
     });
-  }
-
-  async createManyNotifications(data: Prisma.NotificationCreateManyInput[]) {
-    this.pushBatchedNotifications(data);
-    return this.prisma.notification.createMany({
-      data,
-    });
-  }
-
-  async pushSingleNotification(notif: Prisma.NotificationCreateInput) {
-    const targetID = notif.target?.connect?.id;
-    const sourceID = notif.author.connect?.id;
-
-    if (!targetID || !sourceID) {
-      this.logger.error(
-        "You're supposed to always connect users for notifications!",
-      );
-      throw new BadRequestException(
-        "You're supposed to always connect users for notifications!",
-      );
-    }
-
-    const targetUser = await this.users.user({ id: targetID });
-    const authorUser = await this.users.user({ id: sourceID });
-    if (
-      authorUser &&
-      targetUser?.pushToken &&
-      !targetUser.disabledNotifications.includes(notif.source)
-    ) {
-      this.logger.log(`Adding push notification`);
-      return this.pushService.pushData(
-        targetUser.pushToken,
-        targetUser?.sandboxToken ?? false,
-        this.constructNotificationBody(authorUser, targetUser, notif),
-      );
-    }
-  }
-
-  async pushBatchedNotifications(notif: Prisma.NotificationCreateManyInput[]) {
-    const userIDs = [
-      ...new Set(notif.flatMap((each) => [each.targetId, each.authorId])),
-    ];
-
-    const flatMapped = userIDs.flatMap(async (each) => {
-      if (each) {
-        const user = await this.users.user({ id: each });
-        if (!user) {
-          return;
-        }
-        return {
-          id: user?.id,
-          data: user,
-        };
-      }
-    });
-    const users = (
-      await Promise.all(flatMapped.flatMap((each) => each))
-    ).filter(notEmpty);
-
-    return Promise.all(
-      notif.map(async (each) => {
-        const authorUser = users.find(
-          (each2) => each2.id == each.authorId,
-        )?.data;
-        const targetUser = users.find(
-          (each2) => each2.id == each.targetId,
-        )?.data;
-        if (
-          authorUser &&
-          targetUser?.pushToken &&
-          !targetUser.disabledNotifications.includes(each.source)
-        ) {
-          this.logger.log(`Adding push notification`);
-          this.pushService.pushData(
-            targetUser.pushToken,
-            targetUser.sandboxToken ?? false,
-            this.constructNotificationBody(authorUser, targetUser, each),
-          );
-        }
-      }),
-    );
-  }
-
-  constructNotificationBody(
-    author: User,
-    target: User,
-    source: NotificationEitherInterface,
-  ): PushNotifications.Data {
-    const authorName = `${author.firstName}`;
-    let body = '';
-
-    const unreachable = (x: never) => {
-      this.logger.error(`This shouldn't be possible! ${x}`);
-      throw new Error(`This shouldn't be possible! ${x}`);
-    };
-
-    switch (source.source) {
-      case NotificationSource.LIKE_POST:
-        body = `${authorName} liked your post!`;
-        break;
-      case NotificationSource.LIKE_COMMENT:
-        body = `${authorName} commented on your post!`;
-        break;
-      case NotificationSource.COMMENT:
-        body = `${authorName} liked your comment!`;
-        break;
-      case NotificationSource.POST:
-        body = ``;
-        break;
-      case NotificationSource.ADDED_PHOTOS:
-        body = ``;
-        break;
-      case NotificationSource.JOINED_INS:
-        body = ``;
-        break;
-      case NotificationSource.JOIN_INS_REJECTED:
-        body = ``;
-        break;
-      default:
-        unreachable(source.source);
-        break;
-    }
-
-    return {
-      title: 'MyINS',
-      body: body,
-      badge: 1,
-      topic: target.sandboxToken
-        ? 'com.squid40.dev.myins'
-        : 'com.squid40.dev.myins',
-      custom: {
-        ...clean(source),
-      },
-    };
   }
 
   async countUnreadNotifications(
@@ -247,26 +112,4 @@ export class NotificationService {
     }
     return this.prisma.notification.count(data);
   }
-}
-
-interface NotificationEitherInterface {
-  source: NotificationSource;
-  postId?: string | null | undefined;
-  commentId?: string | null | undefined;
-  authorId?: string;
-}
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function clean(obj: any) {
-  for (const propName in obj) {
-    if (obj[propName] === null || obj[propName] === undefined) {
-      delete obj[propName];
-    }
-  }
-  return obj;
-}
-
-function notEmpty<TValue>(value: TValue | null | undefined): value is TValue {
-  if (value === null || value === undefined) return false;
-  return true;
 }
