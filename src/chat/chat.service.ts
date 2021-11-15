ff29f1cc6812cf7eae1808724909d2f37e26385f
Cristi Pele <cristi_pele7@yahoo.com>
2021-11-15 15:31:55 +0200

    remove console.log

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -156,8 +156,6 @@ export class ChatService {
         );
         await this.streamChat.addDevice(deviceToken, 'apn', userID);
       }
-      const devices = await this.streamChat.getDevices(userID);
-      console.log('devices', devices);
     } catch (e) {
       const stringErr: string = <string>e;
       this.logger.error(
