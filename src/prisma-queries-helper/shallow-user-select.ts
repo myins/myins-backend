52ef5878b310c2d33fff14528d4237e0044966f2
Cristi Pele <cristi_pele7@yahoo.com>
2021-11-25 11:39:36 +0200

    userRole for ins members

--- src/prisma-queries-helper/shallow-user-select.ts
+++ src/prisma-queries-helper/shallow-user-select.ts
@@ -7,3 +7,34 @@ export const ShallowUserSelect: Prisma.UserSelect = {
   id: true,
   isDeleted: true,
 };
+
+const shallowUserSelectWithRoleData = (insID: string) => {
+  return {
+    firstName: true,
+    lastName: true,
+    profilePicture: true,
+    id: true,
+    isDeleted: true,
+    inses: {
+      where: {
+        insId: insID,
+      },
+      select: {
+        role: true,
+      },
+    },
+  };
+};
+
+export const ShallowUserSelectWithRoleInclude = (
+  insID: string,
+): Prisma.UserSelect => {
+  return shallowUserSelectWithRoleData(insID);
+};
+
+const shallowUserSelectWithRole = Prisma.validator<Prisma.UserArgs>()({
+  select: shallowUserSelectWithRoleData(''),
+});
+export type ShallowUserSelectWithRole = Prisma.UserGetPayload<
+  typeof shallowUserSelectWithRole
+>;
