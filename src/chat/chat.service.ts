28c8b9da9de6363644075b7238540e1809654a58
Cristi Pele <cristi_pele7@yahoo.com>
2021-12-15 16:21:20 +0200

    fix order media & skip_push for chat messages

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -325,15 +325,19 @@ export class ChatService {
       });
       const user = await this.getStreamUser(userID);
       await Promise.all(
-        channels.map(async (channel) => {
-          await channel.sendMessage({
-            user_id: user.id,
-            text: message,
-            silent: silent ?? false,
+        channels.map(
+          async (channel) => {
+            await channel.sendMessage({
+              user_id: user.id,
+              text: message,
+              silent: silent ?? false,
+              data,
+            });
+          },
+          {
             skip_push: skip_push ?? false,
-            data,
-          });
-        }),
+          },
+        ),
       );
     } catch (e) {
       const stringErr: string = <string>e;
