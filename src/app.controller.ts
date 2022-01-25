24dff135dde8ac703cc9dcf223532c7d1a9f5c28
Cristi Pele <cristi_pele7@yahoo.com>
2022-01-25 15:40:52 +0200

    update .circleci/config.yml

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ export class AppController {
   @Get()
   async getStatus(): Promise<{ status: string }> {
     return {
-      status: 'All good! v7',
+      status: 'All good! Prod -v7',
     };
   }
 }
