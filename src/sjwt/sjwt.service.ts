1183ddbae053befca86fd1e04129f74791ffe005
Cristi Pele <cristian.pele@adonissoft.com>
2022-10-07 11:17:36 +0300

    make cloudfront token 1h long

--- src/sjwt/sjwt.service.ts
+++ src/sjwt/sjwt.service.ts
@@ -32,7 +32,7 @@ export class SjwtService {
 
   getCloudfrontToken(toSign: JwtStrategyPayload) {
     return this.jwtService.sign(toSign, {
-      expiresIn: '15m',
+      expiresIn: '1h',
       secret: process.env.CLOUDFRONT_JWT_KEY,
     });
   }
